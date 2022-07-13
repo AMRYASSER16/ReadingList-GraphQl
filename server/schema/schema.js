6605142eeaf9fc99cc6a7bda3698d9d9d8498174
Amr Yasser <yamr3212@gmail.com>
2022-07-13 19:34:45 +0200

    adding author fields

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,20 +1,33 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 var books = [
-    {
-        id: '1',
-        name: 'book 1',
-        genre: 'book number 1'
-    },
-    {
-        id: '2',
-        name: 'book 2',
-        genre: 'book number 2'
-    }
-]
+	{
+		id: '1',
+		name: 'book 1',
+		genre: 'book number 1'
+	},
+	{
+		id: '2',
+		name: 'book 2',
+		genre: 'book number 2'
+	}
+];
+
+var authors = [
+	{
+		id: '1',
+		name: 'author 1',
+		age: 24
+	},
+	{
+		id: '2',
+		name: 'author 2',
+		age: 26
+	}
+];
 
 const BookType = new GraphQLObjectType({
 	name: 'Book',
@@ -22,6 +35,15 @@ const BookType = new GraphQLObjectType({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString }
+	})
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt }
 	})
 });
 
@@ -35,6 +57,15 @@ const RootQuery = new GraphQLObjectType({
 			},
 			resolve(parentValue, args) {
 				return _.find(books, { id: args.id });
+			}
+		},
+		author: {
+			type: AuthorType,
+			args: {
+				id: { type: GraphQLID }
+			},
+			resolve(parentValue, args) {
+				return _.find(authors, { id: args.id });
 			}
 		}
 	}
