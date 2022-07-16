e5a03efc377f8179964f7e280847195864b79365
Amr Yasser <yamr3212@gmail.com>
2022-07-16 21:47:26 +0200

    fetch books from the server

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_BOOKS = gql`
+	{
+		books {
+			id
+			name
+		}
+	}
+`;
 
 const BookList = () => {
-    return <div>
-        <ul id='book-list'>
-            <li>Book name</li>
-        </ul>
-    </div>;
-}
+	const { loading, error, data } = useQuery(GET_BOOKS);
+
+	useEffect(
+		() => {
+			if (loading === false) {
+				console.log(data);
+			}
+		},
+		[ loading ]
+	);
+
+	return (
+		<div>
+			<ul id="book-list">
+				<li>Book name</li>
+			</ul>
+		</div>
+	);
+};
 
 export default BookList;
