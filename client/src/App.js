1c14a6ef210c588815f666fc74c9e8cd8c4bd67d
Amr Yasser <yamr3212@gmail.com>
2022-07-16 21:29:26 +0200

    adding component to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import BookList from './components/BookList';
+
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Reading List</h1>
+      <BookList />
     </div>
   );
 }
