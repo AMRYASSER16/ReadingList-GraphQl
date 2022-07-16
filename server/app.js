23bd170e24c51ae64af0584abf37ffae6c40f3d0
Amr Yasser <yamr3212@gmail.com>
2022-07-16 18:52:32 +0200

    connect to mongo DB

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
-}))
+}));
+
+mongoose.connect('mongodb+srv://AYasserK:z1mzOMPSjB2Ws1vq@cluster0.i1h7z.mongodb.net/Cluster0?retryWrites=true&w=majority');
+
+mongoose.connection.once('open', () => {
+    console.log('connected to DB')
+})
 
 app.listen(4000, () => console.log('Listening'));
